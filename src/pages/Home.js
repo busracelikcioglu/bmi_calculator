0f672e3941ba98ebf865204bb3325f72084cffa6
Büşra Çelikçioğlu <89510904+busracelikcioglu@users.noreply.github.com>
2024-07-26 00:55:40 +0300

    added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import BMICalculator from '../components/BMICalculator';
-import BMIDescription from '../components/BMIDetails';
 import bmiImage from '../assets/bmi.jpg'; // Resim dosyasının yolu
 
 const Home = () => {
@@ -8,7 +7,6 @@ const Home = () => {
     <div className="container">
       <img src={bmiImage} alt="BMI" className="bmi-image" />
       <BMICalculator />
-      <BMIDescription />
     </div>
   );
 };
