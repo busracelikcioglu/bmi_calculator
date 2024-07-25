a47c08f6829db6074b3d4b5f713fff4204b03715
Büşra Çelikçioğlu <89510904+busracelikcioglu@users.noreply.github.com>
2024-07-26 01:02:56 +0300

    düzenlendi

--- src/pages/Calculations.js
+++ src/pages/Calculations.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Container, Table, Thead, Tbody, Tr, Th, Td, TableCaption } from '@chakra-ui/react';
+import { Container, Table, Thead, Tbody, Tr, Th, Td } from '@chakra-ui/react';
 import { MainContext } from '../context/MainContextProvider';
 
 const Calculations = () => {
